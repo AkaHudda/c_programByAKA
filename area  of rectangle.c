c228664b23032932e08df909af812336d2a6a98a
AkaHudda <109863968+AkaHudda@users.noreply.github.com>
2022-07-26 12:02:31 +0530

    area of rectangle

---
+++ area  of rectangle.c
@@ -0,0 +1,14 @@
+
+//area of rectangle
+
+/*
+*******************************************************************************/
+#include <stdio.h>
+
+
+int main(){
+    int length=3, breadth=8;
+    int area = length*breadth;
+    printf("the area of this rectangle is %d", area);
+    return 0;
+}
